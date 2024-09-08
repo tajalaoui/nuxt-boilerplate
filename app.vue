e324f32307bac0636c68a5c792d0d3799c84f421
tajeddine <tajeddine.zmz.alaoui@gmail.com>
2024-09-08 10:29:45 +0100

    added some css reset

--- app.vue
+++ app.vue
@@ -8,4 +8,20 @@
 body {
   font-family: "Inter", sans-serif !important;
 }
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  text-wrap: balance;
+}
+
+p,
+li,
+figcaption {
+  text-wrap: pretty;
+  max-width: 65ch;
+}
 </style>
